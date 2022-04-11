2352f0b0d53a7ccb2ebb9d2ff80ba651f9cce252
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2022-04-11 19:22:01 +0200

    Delete parking_project/api/Models directory

--- parking_project/api/Models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .parkingLot import ParkingLot
-from .camera import Camera
-from .lotOccupancyHistory import LotOccupancyHistory
-from .netModel import NetModel
-from .parkingSpace import ParkingSpace
-from .spaceOccupancyHistory import SpaceOccupancyHistory
-from .boundingBox import BoundingBox
